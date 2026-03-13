261ad2a2f57498436c1a03c0339bc9cabe301a12
philippark <philippark271@gmail.com>
2026-03-13 01:12:49 -0400

    fix memory safety: use unique ptrs, and consts

--- src/bitbarrel.cc
+++ src/bitbarrel.cc
@@ -9,8 +9,8 @@
 #include <algorithm>
 #include <fstream>
 
-void BitBarrel::load_key_dir_from_segment(Segment *segment) {
-    auto entries = segment->get_all_entries();
+void BitBarrel::load_key_dir_from_segment(const Segment& segment) {
+    auto entries = segment.get_all_entries();
     
     for (const auto& entry : entries) {
         // ignore if earlier entry
@@ -20,16 +20,16 @@ void BitBarrel::load_key_dir_from_segment(Segment *segment) {
             }
         }
         
-        KeyDirEntry kde{segment->get_id(), entry.value_size, 
+        KeyDirEntry kde{segment.get_id(), entry.value_size, 
                         entry.value_pos, entry.timestamp};
         key_dir[entry.key] = kde;
     }
 }
 
-Segment* BitBarrel::create_segment() {
+std::unique_ptr<Segment> BitBarrel::create_segment() {
     uint32_t id = get_current_timestamp();
     std::string file_path = dir_name + "/" + std::to_string(id);
-    return new Segment(id, file_path);
+    return std::make_unique<Segment>(id, file_path);
 }
 
 BitBarrel::BitBarrel(const std::string& dir_name) {
@@ -43,25 +43,26 @@ BitBarrel::BitBarrel(const std::string& dir_name) {
     // rebuild key dir and in-mem list of segments
     for (auto& file : files) {
         uint32_t segment_id = static_cast<uint32_t>(std::stoi(file));
-        Segment *segment = new Segment(segment_id, dir_name + "/" + file);
-        data_store.push_back(segment);
-        load_key_dir_from_segment(segment);
+        auto segment = std::make_unique<Segment>(segment_id, dir_name + "/" + file);
+        load_key_dir_from_segment(*segment);
+        data_store.push_back(std::move(segment));
     }
 
     if (data_store.empty()) {
         // Create initial segment
-        Segment *segment = create_segment(); 
-        data_store.push_back(segment);
+        auto segment = create_segment();
+        data_store.push_back(std::move(segment));
     } 
 
-    active_segment = data_store.back();
+    active_segment = data_store.back().get();
 }
 
 Status BitBarrel::set(std::string key, std::string value) {
     std::cout << "active segment size: " << active_segment->get_size() << "\n";
     if (active_segment->is_full(key.size() + value.size())) {
-        active_segment = create_segment();
-        data_store.push_back(active_segment); 
+        auto new_segment = create_segment();
+        data_store.push_back(std::move(new_segment));
+        active_segment = data_store.back().get();
     }
 
     Result<uint32_t> res = active_segment->set(key, value); 
